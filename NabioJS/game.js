021d731e4bf8c944cf220e7ece4bdea22a16ae6e
Xiryl <fabio@chiarani.it>
2016-01-13 17:37:08 +0100

    Add (incomplete) mrsnake object

--- NabioJS/game.js
+++ NabioJS/game.js
@@ -74,3 +74,103 @@ game = {
 };
 
 game.drawScore();
+
+
+
+
+
+
+/*
+============
+OGGETTO SNAKE
+============
+
+
+-SIZE : grandezza snake
+-x,y per posizione
+-COLOR: colore
+-NAME: soprannome
+-DIRECTION: lo gestisco per singolo snake --> always start on left
+-SECTIONS: sezioni (array di cubi)
+
+*/
+snake = {
+
+    size: canvas.width / 40,
+    x: null,
+    y: null,
+    color: 'yellow',
+    name: 'MrSnake',
+    direction: 'left',
+    sections: [],
+
+    init: function () {
+        snake.sections = [];
+        snake.direction = 'left';
+        //start in centro del canvas
+        snake.x = canvas.width / 2 + snake.size / 2;
+        snake.y = canvas.height / 2 + snake.size / 2;
+        for (i = snake.x + (5 * snake.size) ; i >= snake.x; i -= snake.size) {
+            snake.sections.push(i + ',' + snake.y);
+        }
+    },
+
+    move: function () {
+        switch (snake.direction) {
+            case 'up':
+                snake.y -= snake.size;
+                break;
+            case 'down':
+                snake.y += snake.size;
+                break;
+            case 'left':
+                snake.x -= snake.size;
+                break;
+            case 'right':
+                snake.x += snake.size;
+                break;
+        }
+        snake.checkCollision();
+        snake.checkGrowth();
+        snake.sections.push(snake.x + ',' + snake.y);
+    },
+
+    draw: function () {
+        for (i = 0; i < snake.sections.length; i++) {
+            snake.drawSection(snake.sections[i].split(','));
+        }
+    },
+
+    drawSection: function (section) {
+        game.drawBox(parseInt(section[0]), parseInt(section[1]), snake.size, snake.color);
+    },
+
+    checkCollision: function () {
+        if (snake.isCollision(snake.x, snake.y) === true) {
+            game.stop();
+        }
+    },
+
+    isCollision: function (x, y) {
+        if (x < snake.size / 2 ||
+            x > canvas.width ||
+            y < snake.size / 2 ||
+            y > canvas.height ||
+            snake.sections.indexOf(x + ',' + y) >= 0) {
+            return true;
+        }
+    },
+
+    checkGrowth: function () {
+        if (snake.x == food.x && snake.y == food.y) {
+            game.score++;
+            if (game.score % 5 == 0 && game.fps < 60) {
+                game.fps++;
+            }
+            food.set();
+        } else {
+            snake.sections.shift();
+        }
+    }
+
+};
